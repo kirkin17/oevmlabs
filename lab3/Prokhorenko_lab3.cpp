c88617230e2358233b728476715a169acbb11df9
kirkin <kirkin.kp@gmail.com>
2022-12-10 12:42:02 +0300

    lab3 done

--- lab3/Prokhorenko_lab3.cpp
+++ lab3/Prokhorenko_lab3.cpp
@@ -16,9 +16,11 @@ int main()
     int Y0 = Ymax - 30;
 	int X1 = Xmax - 30;
     int Y1 = 30;
-	int N = X1 - X0;
+	int NX = X1 - X0;
+	int NY = Y0 - Y1;
 	
-    char Ox[9][4] = {"1PI", "2PI", "3PI", "4PI", "5PI", "6PI", "7PI", "8PI", "9PI"};
+    char ox[9][4] = {"1PI", "2PI", "3PI", "4PI", "5PI", "6PI", "7PI", "8PI", "9PI"};
+	char oy[10][3] = {"0", "-1", "-2", "-3", "-4", "-5", "-6", "-7", "-8", "-9"};
 	setlinestyle(0,1,1); 
 	outtextxy(200, 400, "f(x)=cos^3(x/2) - sqrt(x) on PI to 9PI");
 	
@@ -28,41 +30,46 @@ int main()
 
 	outtextxy(X0, Y0, "f(x)");
 	outtextxy(X1 + 3, Y1, "x");
-	outtextxy(X0 - 30, Y1 - 20, "0");
-	outtextxy(X0 - 10, Y0 - 20, "1");
 		
 	for(int i = 0; i < 10; i++)	//Рисуем метки и подписи на оси Х
 	{
-		line(X0 + (int)(N/8) * i, Y1-3, X0 + (int)(N/8) * i, Y1+3); //Метки
-		outtextxy(X0 + (int)(N/8) * i, Y1-20, Ox[i]); 	//Подписи
-	};
+		line(X0 + (int)(NX/8) * i, Y1-3, X0 + (int)(NX/8) * i, Y1+3); //Метки
+		outtextxy(X0 + (int)(NX/8) * i, Y1-20, ox[i]); 	//Подписи
+	}
+
+	for(int i = 0; i <= 9; i++)	//Рисуем метки и подписи на оси Y
+	{
+		line(X0 - 3, Y1 + (int)(NY/9) * i, X0 + 3, Y1 + (int)(NY/9) * i); //Метки
+		outtextxy(X0 - 20, (int)(NY/9) * i + 25, oy[i]); 	//Подписи
+	}
 
 	const double PI = 3.1415926535;
-    double result[N] = {0};
+    double result[NX] = {0};
 	double x = PI;
 	double xmax = 9 * PI;
-    double dx = (xmax - x) / N;
+    double dx = (xmax - x) / NX;
     double max = -10.;
-	for (int i = 0; i < N; i++)
+	for (int i = 0; i < NX; i++)
 	{
 		result[i] = (double)(pow(cos(x/2), 3) - sqrt(x));  //сама функция
 		if(max < result[i]) max = result[i];
         x += dx;
     }
 	
-    int normalizedDots[N] = {0};
-	for (int i = 0; i < N; i++)
+    int normalizedDots[NX] = {0};
+	for (int i = 0; i < NX; i++)
     {
-		normalizedDots[i] = (int)(result[i] / (result[0] + result[N-1]) * (double)(Y0 - Y1));
+		normalizedDots[i] = (int)(result[i] / (result[0] + result[NX-1]) * (double)(Y0 - Y1));
 	}
 	// Рисуем график функции
-	for(int i = 0; i < N; ++i)
+	for(int i = 0; i < NX; ++i)
 	{
 		putpixel(X0+i, normalizedDots[i], 14);  // 12 - это цвEт линии графика
 		//delay(10);
 	}
 		
-	outtextxy(200, 50, "Maximum of F(x) is ________________");
+	outtextxy(200, 420, "Maximum of F(x) is -1.7");
+	std::cout << max;
 	getch();
 	closegraph();
 	getch();  
--- lab3/test.cpp
+++ lab3/test.cpp
@@ -7,32 +7,31 @@ int main()
 {
     int driver = DETECT, mode;
     initgraph(&driver, &mode, "c://turboc3//bgi");
-    int Xmax = getmaxx();
-    int Ymax = getmaxy();
-    int X1 = 40;
-    int X2 = Xmax - 40;
-    int Y1 = 50;
-    int Y2 = Ymax - 70;
-    int N = X2 - X1 - 47 - 26;
+    int Xmax = getmaxx(); 
+	int Ymax = getmaxy(); 
+	int X0 = 40;
+    int Y0 = Ymax - 30;
+	int X1 = Xmax - 30;
+    int Y1 = 30;
+	int NX = X1 - X0;
+	int NY = Y0 - Y1;
 
-    int i;
-    for (i = 1; i < 19; ++i)
-        line(X1 - 5, Y2 - i * 20, X1 + 5, Y2 - i * 20);
-    for (i = 1; i < 17; ++i)
-        line(X1 + 35 * i, Y2 + 5, X1 + 35 * i, Y2 - 5);
-    
-    char xi[14][2];
-    for (i = 4; i < 61; i += 4)
-    {
-        sprintf(xi[i / 4 - 1], "%dp/2", i/4);
-        outtextxy(X1 + 15.45 * i / 2 + i - 3, Y2 + 7, xi[i / 4 - 1]);
-    }
+    line(X0, Y0, X0, Y1);
+	line(X0, Y1, X1, Y1);
+    char ox[9][4] = {"1PI", "2PI", "3PI", "4PI", "5PI", "6PI", "7PI", "8PI", "9PI"};
+	char oy[7][1] = {'0', '1', '2', '3', '4', '5', '6'};
+    // for(int i = 0; i < 10; i++)	//Рисуем метки и подписи на оси Х
+	// {
+	// 	line(X0 + (int)(NX/8) * i, Y1-3, X0 + (int)(NX/8) * i, Y1+3); //Метки
+	// 	outtextxy(X0 + (int)(NX/8) * i, Y1-20, ox[i]); 	//Подписи
+	// }
 
-    char yi[14][2];
-    for (i = 2; i < 19; i += 2)
-    {
-        sprintf(yi[i / 2 - 1], "%d", i / 2);
-        outtextxy(X1 - 20, Y2 - 20 * i - i / 4, yi[i / 2 - 1]);
-    }
+	for(int i = 0; i <= 6; i++)	//Рисуем метки и подписи на оси Y
+	{
+		line(X0 - 3, Y1 + (int)(NY/6) * i, X0 + 3, Y1 + (int)(NY/6) * i); //Метки
+		//outtextxy(X0 - 20, (int)(NY/6) * i, oy[i]); 	//Подписи
+	}
+
+
     getch();
 }
